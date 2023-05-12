59ffcc70008d0d4f7dc4e15caa6f3e155ee33027
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-05-12 11:39:54 +0300

    base adding

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
   message: {
     type: String,
